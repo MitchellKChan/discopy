8e4fe53c3fa8a3af590ce706c91f5d78ed077b6b
Mitchell Chan <mitchellkchan@gmail.com>
2023-10-03 23:42:46 -0700

    complete backend user authentication API setup

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -5,9 +5,9 @@ class Api::UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save # check validations, save to db
+    if @user.save
       login!(@user)
-      render :show # send jbuilder response
+      render :show
     else
       render json: {errors: @user.errors.full_messages}, status: 422
     end
---
+++ app/views/api/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :email, :username, :display_name, :created_at
