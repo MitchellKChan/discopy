b25413e9c0b35df2658f467f992647bd5539b7c3
Mitchell Chan <mitchellkchan@gmail.com>
2023-10-11 10:44:19 -0700

    clean up servers and joined_servers migrations; add joined_servers seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,55 +17,29 @@ ApplicationRecord.transaction do
   
     puts "Creating users..."
     # Create some initial users with usernames, display_names, emails, passwords, and statuses:
-    User.create!(
-        username: 'mitchibonbon',
-        email: 'mitchi@bon.bon',
-        password: 'a10ng3RanDm0r35ECurEpA55w0Rd',
-        status: 'Offline'
-    )
-
-    User.create!(
-      username: 'DemoUser',
-      display_name: 'demo-lition',
-      email: 'discopydemo@user.io',
-      password: 'password',
-      status: 'Offline'
-    )
-
-    User.create!(
-      username: 'Baymax',
-      display_name: 'Personal Healthcare Companion',
-      email: 'baymax@bighero6.io',
-      password: 'badaladaladala',
-      status: 'Offline'
-    )
-
-    User.create!(
-      username: 'myspacetom',
-      display_name: 'Tom',
-      email: 'tom@my.space',
-      password: 'password',
-      status: 'Offline'
-    )
+    User.create!(username: 'mitchibonbon',email: 'mitchi@bon.bon',password: 'a10ng3RanDm0r35ECurEpA55w0Rd',status: 'Offline')
+    User.create!(username: 'DemoUser',display_name: 'demo-lition',email: 'discopydemo@user.io',password: 'password',status: 'Offline')
+    User.create!(username: 'Baymax',display_name: 'Personal Healthcare Companion',email: 'baymax@bighero6.io',password: 'badaladaladala',status: 'Offline')
+    User.create!(username: 'myspacetom',display_name: 'Tom',email: 'tom@my.space',password: 'password',status: 'Offline')
 
     puts "Creating servers..."
     # Create some initial servers with names, creator_ids, and public fields:
-    Server.create!(
-      name: "Big Hero 6 Fan Club",
-      creator_id: 3,
-      public: true
-    )
+    Server.create!(name: "Big Hero 6 Fan Club",creator_id: 3,public: true)
+    Server.create!(name: "Myspace",creator_id: 4,public: true)
 
-    Server.create!(
-      name: "Myspace",
-      creator_id: 4,
-      public: true
-    )
+    puts "Creating joined_servers..."
+    # Create some initial joined_servers with server_ids and member_ids:
+    JoinedServer.create!(server_id: 1,member_id: 1)
+    JoinedServer.create!(server_id: 1,member_id: 2)
+    JoinedServer.create!(server_id: 1,member_id: 3)
+    JoinedServer.create!(server_id: 1,member_id: 4)
+    JoinedServer.create!(server_id: 2,member_id: 2)
+    JoinedServer.create!(server_id: 2,member_id: 4)
   
     # More users; may not need in final version but keeping for reference
     # 5.times do 
     #   User.create!({
-    #     username: Faker::Internet.unique.username(specifier: 3),
+    #     username: Faker::Internet.unique.username(specifier),
     #     display_name: Faker::Internet.unique.username(specifier: 3),
     #     email: Faker::Internet.unique.email,
     #     password: 'password',
