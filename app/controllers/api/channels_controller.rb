8f46fc9ad651d84055b8a39ddc2031b7f520d421
Mitchell Chan <mitchellkchan@gmail.com>
2023-10-17 08:54:01 -0700

    test and validate backend channels_controller routes are working via browser devtools

--- app/controllers/api/channels_controller.rb
+++ app/controllers/api/channels_controller.rb
@@ -3,4 +3,33 @@ class Api::ChannelsController < ApplicationController
 
     wrap_parameters include: Channel.attribute_names + ["serverId"]
 
+    def create
+        @channel = Channel.new(channels_params)
+        if @channel.save
+            render :show
+        else
+            render json: {errors: @channel.errors.full_messages}, status: 422
+        end
+    end
+
+    def update
+        @channel = Channel.find_by(id: params[:id])
+        if @channel.update(channels_params)
+            render :show
+        else
+            render json: {errors: @channel.errors.full_messages}, status: 422
+        end
+    end
+
+    def destroy
+        @channel = Channel.find_by(id: params[:id])
+        channelId = @channel.id
+        @channel.destroy
+        render json: { channelId: channelId }
+    end
+
+    private
+    def channels_params
+        params.require(:channel).permit(:name, :server_id)
+    end
 end
